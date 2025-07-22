cfb5dca8d3df09473714e96452b2baca9ef1433f
Bola Lokesh <loki944818@gmail.com>
2025-07-22 13:07:42 +0530

    Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import api from '../api';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    await api.post('/login', { username, password });
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;
