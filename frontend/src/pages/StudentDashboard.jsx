ca4f63dae442bd02516f1c5ed2e92ff588d71136
Bola Lokesh <loki944818@gmail.com>
2025-07-22 13:09:25 +0530

    StudentDashboard.jsx

---
+++ frontend/src/pages/StudentDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SubmissionForm from '../components/SubmissionForm';
+
+const StudentDashboard = () => (
+  <div>
+    <h2>Student Dashboard</h2>
+    <SubmissionForm />
+  </div>
+);
+
+export default StudentDashboard;
